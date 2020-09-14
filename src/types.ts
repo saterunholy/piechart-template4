b6af3b47515de506fd1eafca95b3be0c2298c9e4
Jose Moreno <58490017+saterunholy@users.noreply.github.com>
2020-09-14 13:14:58 -0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -92,7 +92,7 @@ return {
             type: 'pie',
             radius: ['40%', '55%'],
             label: {
-                formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c|{c}}  {per|{d}%}  ',
+                formatter: '{a|{a}}{abg|}\\n{hr|}\\n  {b|{b}：}{c|{c}}  {per|{d}%}  ',
                 backgroundColor: '#eee',
                 borderColor: '#aaa',
                 borderWidth: 1,
